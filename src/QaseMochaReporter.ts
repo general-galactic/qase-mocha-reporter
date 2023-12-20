3a439bf98d294f529063a79eb65f2925d3426c34
jkennedy1980 <josh@deadmeta4.com>
2023-12-20 15:57:46 -0800

    loging

--- src/QaseMochaReporter.ts
+++ src/QaseMochaReporter.ts
@@ -245,7 +245,7 @@ export class QaseMochaReporter extends reporters.Base {
         })
 
         try {
-            debug(`Uploading qase ${qaseResults.length} test results: `)
+            debug(`Uploading qase ${qaseResults.length} test results for run id=${this.qaseTestRunId}: `)
             debug('CASE RESULT JSON', JSON.stringify(qaseResults, null, '\t'))
             const result = await this.qase.results.createResultBulk(this.qaseProjectCode, this.qaseTestRunId, {
                 results: qaseResults
